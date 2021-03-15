b8dee924e355a7ff0bd13a0fdb0657fcda34b995
JFreud <jrm.frdnbrg@gmail.com>
2021-03-15 13:49:20 -0400

    posterior mode instead of highest prob

--- helper.R
+++ helper.R
@@ -6,12 +6,19 @@
 #' @param niter number of iterations for the gibbs sampling
 get_cluster_assignments <- function(data, x, res, niter=100) {
   p_zmax <- apply(res$p.z.given.x[,,niter], 2, which.max)
+  z_assignments <- apply(res$z[-(1:50),], 2, get_mode)
   # assign to cluster with highest probability
   assignments <- data.frame(
     ID=rownames(x),
-    assignment=p_zmax)
+    assignment=z_assignments)
   assignments <- cbind(assignments, t(res$p.z.given.x[,,niter]))
   assignments$POS <- sapply(assignments$ID,function(x) {df[df$ID==x,]$POS[1]})
   assignments$POS <- unlist(assignments$POS)
   return(assignments)
+}
+
+
+get_mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
 }
