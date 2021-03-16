085b1bf78d0acd2f7e0c221be14242e3b2166e44
JFreud <jrm.frdnbrg@gmail.com>
2021-03-15 21:54:09 -0400

    triangle plots

--- plot_functions.R
+++ plot_functions.R
@@ -42,3 +42,39 @@ plot_gibbs_chain <- function(res, K) {
     }
   }
 }
+
+#' @param trait_prop R by K matrix or df of proportions of variants in cluster for each trait
+plot_triangles <- function(trait_prop) {
+  triangle_list <- list()
+  for (i in 1:nrow(t_prop)) {
+    clusters <- which(t_prop[i,] > 0.1)
+    if (length(clusters) < 3) {
+      clusters <- sort(order(t_prop[i,], decreasing=T)[1:3])
+    }
+    triangles <- t(combn(clusters,3))
+    # add to traits in triangles
+    for (j in 1:nrow(triangles)) {
+      name <- paste(triangles[j,], collapse='')
+      if (name %in% names(triangle_list)) {
+        # add cluster values to triangle
+        triangle_list[[name]] <- rbind(triangle_list[[name]], t_prop[i,triangles[j,]])
+      } else {
+        # create new triangle
+        triangle_list[[name]] <- t_prop[i,triangles[j,]]
+      }
+    }
+  }
+  p_list <- lapply(triangle_list, function(triangle) {
+    curr_set <- triangle %>% tibble::rownames_to_column("trait")
+    colnames(curr_set) <- c("trait", "x", "y", "z")
+    p <- ggtern(curr_set,aes(x,y,z,label=trait)) +
+      geom_point(size=1.2, aes(color=rgb(x,y,z)),show.legend=FALSE) +
+      geom_text(vjust=1, size=2, aes(color=rgb(x,y,z)),show.legend=F, srt=20) +
+      labs(x="1",y="3",z="5",
+           xarrow="% variants in cluster 1", yarrow="% variants in cluster 3", zarrow="% variants in cluster 5") +
+      theme_showarrows() +
+      theme_nomask()
+    return(p)
+  })
+  return(p_list)
+}
